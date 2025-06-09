d638b6c57b2838be793749985630bd7e62d9a865
Flavio <77132531+flaviodev27@users.noreply.github.com>
2025-06-09 20:47:31 -0300

    feat: add instruments enum

---
+++ app/Enums/Instrumentos.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Enums;
+
+enum Instrumentos: string
+{
+    case Bandolim = 'Bandolim';
+    case Canto = 'Canto';
+    case Cavaquinho = 'Cavaquinho';
+    case Clarineta = 'Clarineta';
+    case Composicao = 'Composição';
+    case ContraBaixo = 'ContraBaixo';
+    case Cravo = 'Cravo';
+    case Fagote = 'Fagote';
+    case Flauta = 'Flauta';
+    case Harpa = 'Harpa';
+    case Oboe = 'Oboe';
+    case Orgão = 'Órgão';
+    case Percussão = 'Percussão';
+    case Piano = 'Piano';
+    Case RegenciaCoral = 'Regência Coral';
+    case RegenciaBando = 'Regência Banda';
+    Case RegenciaOrquestra = 'Regência Orquestral';
+    case Saxofone = 'Saxofone';
+    case Trombone = 'Trombone';
+    case Trompa = 'Trompa';
+    case Trompete = 'Trompete';
+    case Tuba = 'Tuba';
+    Case Viola = 'Viola';
+    case Violao = 'Violão';
+    case Violino = 'Violino';
+    case Violoncelo = 'Violoncelo';
+}

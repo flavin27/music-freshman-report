889bb7af1b6f032bdca704f8bc1b88486cde0818
Flavio <77132531+flaviodev27@users.noreply.github.com>
2025-06-12 23:11:59 -0300

    feat: import data command almost done!

--- app/Console/Commands/ImportEntranceExamResult.php
+++ app/Console/Commands/ImportEntranceExamResult.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
 class ImportEntranceExamResult extends Command
 {
@@ -11,7 +12,7 @@ class ImportEntranceExamResult extends Command
      *
      * @var string
      */
-    protected $signature = 'app:import {url}';
+    protected $signature = 'app:import {semester} {phase} {url}';
 
     /**
      * The console command description.
@@ -26,14 +27,44 @@ class ImportEntranceExamResult extends Command
     public function handle()
     {
         $url = $this->argument('url');
+		$semester = $this->argument('semester');
+		$phase = $this->argument('phase');
 
         if (!filter_var($url, FILTER_VALIDATE_URL)) {
             $this->error('Invalid URL provided.');
             return 1;
         }
 
-        $this->info("Processing URL: {$url}");
+        $this->info("Processing URL...");
+		
+		$file = file_get_contents($url);
+		
+		$fileName = "public/pdfs/result-{$semester}-{$phase}.pdf";
+		
+		Storage::put($fileName, $file);
+		
+		$path = Storage::path($fileName);
+		
+		$this->info("Downloaded completed!");
+		
+		$this->info("Extracting data...");
+		
+		$output = shell_exec("pdftotext -layout {$path} -");
+		
+		$notas = explode("\n", $output);
+		
+		$dados = [];
 
+		foreach ($notas as $nota ){
+			$aluno = preg_split('/\s{2,}/', $nota);
+
+			if (count($aluno) > 4 && !empty($aluno[0])) {
+				$dados[] = $aluno;
+			}
+		}
+		
+		print_r($dados);
+		
         $this->info('Import completed successfully.');
         return 0;
     }
